142827c2c660ecb45ea65fcfa095de638e057336
RendL <farukh-999@mail.ru>
2020-02-26 23:18:28 +0500

    Dsds

--- client/client-cli.go
+++ client/client-cli.go
@@ -1,7 +1,187 @@
 package main
 
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net"
+	"os"
+)
+
 func main() {
+	list := flag.Bool("list", false, "get directory list")
+	upload := flag.Bool("upload", false, "get directory list")
+	download := flag.Bool("download", false, "get directory list")
 
+	file, err := os.OpenFile("client-log.txt", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
+	if err != nil {
+		fmt.Printf("can't start application client%e", err)
+		return
+	}
+	defer func() {
+		err := file.Close()
+		if err != nil {
+			fmt.Printf("can't close file %e", err)
+		}
+	}()
 
+	log.SetOutput(file)
+	flag.Parse()
+
+	conn, err := net.Dial("tcp", "localhost:9999")
+	if err != nil {
+		log.Fatalf("can't connect to 0.0.0.0:9999 %v", err)
+		fmt.Println("Сервер не найден")
+		return
+	}
+	log.Print("Start application client")
+	defer func() {
+		err = conn.Close()
+		if err != nil {
+			log.Printf("can't close connection %v", err)
+		}
+	}()
+	reader, writer := bufio.NewReader(conn), bufio.NewWriter(conn)
+	if *list {
+		_, err = writer.Write([]byte("LIST\n"))
+		if err != nil {
+			log.Printf("can't write to localhost:9999 %v", err)
+			fmt.Println("Не удалось отправить запрос серверу")
+			return
+		}
+		err = writer.Flush()
+		if err != nil {
+			log.Printf("can't write to localhost:9999 %v", err)
+			fmt.Println("Не удалось отправить запрос серверу")
+			return
+		}
+		counter := 0
+		fmt.Println("files:")
+		for {
+			readString, err := reader.ReadString('\n')
+			if err == io.EOF {
+				fmt.Printf("Total: %d files", counter)
+				return
+			}
+			if err != nil {
+				log.Printf("can't read frocm loalhost %v", err)
+				fmt.Println("Не удалось получить ответ")
+				return
+			}
+			counter++
+			fmt.Println(readString)
+		}
+	}else if *download {
+		_, err = writer.Write([]byte("DOWNLOAD\n"))
+		if err != nil {
+			log.Printf("can't write to localhost:9999 %v", err)
+			fmt.Println("Не удалось отправить запрос серверу")
+			return
+		}
+		err = writer.Flush()
+		fmt.Println("send download")
+		if err != nil {
+			log.Printf("can't write to localhost:9999 %v", err)
+			fmt.Println("Не удалось отправить запрос серверу")
+			return
+		}
+
+		_, err = writer.Write([]byte(os.Args[2]+"\n"))
+		if err != nil {
+			log.Printf("can't write to localhost:9999 %v", err)
+			fmt.Println("Не удалось отправить запрос серверу")
+			return
+		}
+		err = writer.Flush()
+
+		if err != nil {
+			log.Printf("can't write to localhost:9999 %v", err)
+			fmt.Println("Не удалось отправить запрос серверу")
+			return
+		}
+		fmt.Println("send file name")
+		file, err := os.Create("client/"+os.Args[2])
+		if err != nil {
+			log.Printf("can't create file %v", err)
+			fmt.Println("Не удалось создать файл")
+			return
+		}
+		defer func() {
+			err = file.Close()
+			if err != nil {
+				log.Printf("can't close file %v", err)
+			}
+		}()
+		fmt.Println("start reading")
+		bytes, err := ioutil.ReadAll(reader)
+		if err != nil {
+			log.Printf("can't write to file %v", err)
+			fmt.Println("Не удалось записать в файл")
+			return
+		}
+
+		_, err = file.Write(bytes)
+		if err != nil {
+			log.Printf("can't write to file %v", err)
+			fmt.Println("Не удалось записать в файл")
+			return
+		}
+	}else if *upload{
+		_, err = writer.Write([]byte("UPLOAD\n"))
+		if err != nil {
+			log.Printf("can't write to localhost:9999 %v", err)
+			fmt.Println("Не удалось отправить запрос серверу")
+			return
+		}
+		err = writer.Flush()
+		if err != nil {
+			log.Printf("can't write to localhost:9999 %v", err)
+			fmt.Println("Не удалось отправить запрос серверу")
+			return
+		}
+
+		_, err = writer.Write([]byte(os.Args[2]+"\n"))
+		if err != nil {
+			log.Printf("can't write to localhost:9999 %v", err)
+			fmt.Println("Не удалось отправить запрос серверу")
+			return
+		}
+		err = writer.Flush()
+
+		if err != nil {
+			log.Printf("can't write to localhost:9999 %v", err)
+			fmt.Println("Не удалось отправить запрос серверу")
+			return
+		}
+		file, err := os.Open("client/" + os.Args[2])
+		if err != nil {
+			log.Printf("can't open file %v", err)
+			fmt.Println("Не удалось открыть файл")
+			return
+		}
+		bytes, err := ioutil.ReadAll(file)
+		if err != nil {
+			log.Printf("can't read file %v", err)
+			fmt.Println("Не удалось прочитать файл")
+			return
+		}
+
+		_, err = writer.Write(bytes)
+		if err != nil {
+			log.Printf("can't write to localhost %v", err)
+			fmt.Println("Не удалось отправить файл")
+			return
+		}
+		err = writer.Flush()
+		if err != nil {
+			log.Printf("can't write to localhost %v", err)
+			fmt.Println("Не удалось отправить файл")
+			return
+		}
+	}
 
 }
+
--- server/server-cli.go
+++ server/server-cli.go
@@ -1,7 +1,140 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"os"
+)
+
 func main() {
+	file, err := os.OpenFile("server-log.txt", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
+	if err != nil {
+		fmt.Printf("can't start application server%e", err)
+		return
+	}
+	defer func() {
+		err := file.Close()
+		if err != nil {
+			log.Printf("can't close file %e", err)
+		}
+	}()
 
+	log.SetOutput(file)
+	log.Print("start application\n")
+	host := "0.0.0.0"
+	port, ok := os.LookupEnv("PORT")
+	if !ok {
+		port = "9999"
+	}
+	fmt.Println(port)
 
+	err = startServer(fmt.Sprintf("%s:%s", host, port))
+	if err != nil {
+		log.Printf("some error in server %e", err)
+	}
 
+}
+
+func startServer(address string) error {
+	listener, err := net.Listen("tcp", address)
+	if err != nil {
+		return fmt.Errorf("can't listen: %v", err)
+	}
+	defer func() {
+		err = listener.Close()
+		if err != nil {
+			err = fmt.Errorf("can't close Listener %e", err)
+		}
+	}()
+	fmt.Println("listen...")
+	for {
+
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Printf("can't accept client %e", err)
+		}
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	fmt.Println("some connection...")
+	defer func() {
+		err := conn.Close()
+		if err != nil {
+			log.Printf("can't close connection %e", err)
+		}
+	}()
+
+	reader, write := bufio.NewReader(conn), bufio.NewWriter(conn)
+	for {
+		str, err := reader.ReadString('\n')
+		if err != nil {
+			log.Printf("can't read command %e", err)
+			return
+		}
+		dir, err := ioutil.ReadDir("downloads")
+		if err != nil {
+			log.Printf("can't read directory <downloads> %e", err)
+			return
+		}
+		if str == "LIST\n" {
+			for _, info := range dir {
+				if !info.IsDir() {
+					_, err = write.Write([]byte(info.Name() + "\n"))
+					if err != nil {
+						log.Printf("can't write to client %e", err)
+						return
+					}
+					err = write.Flush()
+					if err != nil {
+						log.Printf("can't write to client %e", err)
+						return
+					}
+				}
+			}
+			return
+		} else if str == "DOWNLOAD\n" {
+			fileName := ""
+			fmt.Printf("download %s", fileName)
+				fileName, err = reader.ReadString('\n')
+
+				if err != nil {
+					log.Printf("can't read command %e", err)
+					return
+				}
+				if fileName == ""{
+					return
+				}
+
+			fmt.Printf("download %s", fileName)
+			for _, info := range dir {
+				if !info.IsDir() && info.Name()+"\n" == fileName {
+					fmt.Println("find")
+					bytes, err := ioutil.ReadFile("downloads/"+info.Name())
+					if err != nil {
+						log.Printf("can't read file %e", err)
+						return
+					}
+					_, err = write.Write(bytes)
+					if err != nil {
+						log.Printf("can't write to client %e", err)
+						return
+					}
+					err = write.Flush()
+					if err != nil {
+						log.Printf("can't write to client %e", err)
+						return
+					}
+					return
+				}
+			}
+		} else if str == "UPLOAD\n" {
+
+		}
+		return
+	}
 }
